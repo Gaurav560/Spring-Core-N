28854839634c44228223eae043a0112cff8bf1ef
GAURAV SHARMA <gsharma1154@gmail.com>
2023-05-12 18:40:13 +0530

    advancedBeanFactory

---
+++ ConstructorInjectionAdvancedBeanFactory-8/src/in/ineuron/test/AdvancedBeanFactoryApp.java
@@ -0,0 +1,41 @@
+package in.ineuron.test;
+
+
+
+
+import java.io.IOException;
+
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+
+
+
+import in.ineuron.component.Student;
+
+//no resource object is used to load the xml file configuration 
+public class AdvancedBeanFactoryApp {
+	
+	public static void main(String[] args) throws Exception {
+		DefaultListableBeanFactory dFactory=null;
+	XmlBeanDefinitionReader reader =null;
+
+		dFactory=new DefaultListableBeanFactory();
+		reader=new XmlBeanDefinitionReader(dFactory);
+		reader.loadBeanDefinitions("in/ineuron/cfg/applicationContext.xml");
+		System.out.println("*************Container Started***************\n");
+		
+		System.in.read();
+		
+		Student student=dFactory.getBean("Student",Student.class);
+		System.out.println(student);
+		System.out.println("\n*******************Container Closed*******************");
+		
+		
+		
+	}
+	
+}
+
+
+
+
